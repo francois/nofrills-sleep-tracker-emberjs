09558db470a058ef0cb3d17dbad69d3b91a217be
François Beausoleil <francois@teksol.info>
2017-01-06 23:49:43 -0500

    Provide minimal statistics for average night

---
+++ app/controllers/home.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  averageNapDuration: Ember.computed('model', function() {
+    let napEvents = this.get('model').
+      filter(function(event) { return event.type === 'nap'; });
+
+    let totalDuration = napEvents.
+      map(function(event) { return event.durationInSeconds; }).
+      reduce(function(a, b) { return a + b; }, 0);
+
+    return totalDuration / napEvents.length;
+  }),
+
+  averageNightDuration: Ember.computed('model', function() {
+    let nightEvents = this.get('model').
+      filter(function(event) { return event.type === 'night'; });
+
+    let totalDuration = nightEvents.
+      map(function(event) { return event.durationInSeconds; }).
+      reduce(function(a, b) { return a + b; }, 0);
+
+    return totalDuration / nightEvents.length;
+  }),
+});
--- app/routes/home.js
+++ app/routes/home.js
@@ -2,39 +2,39 @@ import Ember from 'ember';
 
 let events = [
   {
-    "dow": 5,
     "durationInSeconds": 29280,
     "endAt": 1471087260000,
     "startAt": 1471057980000,
-    "timezone": "America/Montreal"
+    "timezone": "America/Montreal",
+    "type": "night"
   },
   {
-    "dow": 0,
     "durationInSeconds": 28980,
     "endAt": 1471260060000,
     "startAt": 1471231080000,
-    "timezone": "America/New_York"
+    "timezone": "America/New_York",
+    "type": "night"
   },
   {
-    "dow": 1,
     "durationInSeconds": 28680,
     "endAt": 1471346460000,
     "startAt": 1471317780000,
-    "timezone": "America/Montreal"
+    "timezone": "America/Montreal",
+    "type": "night"
   },
   {
-    "dow": 3,
-    "durationInSeconds": 23640,
-    "endAt": 1471432860000,
+    "durationInSeconds": 34440,
+    "endAt": 1471443660000,
     "startAt": 1471409220000,
-    "timezone": "America/Montreal"
+    "timezone": "America/Montreal",
+    "type": "night"
   },
   {
-    "dow": 2,
     "durationInSeconds": 2520,
     "endAt": 1471373400000,
     "startAt": 1471370880000,
-    "timezone": "America/Montreal"
+    "timezone": "America/Montreal",
+    "type": "nap"
   }
 ];
 
