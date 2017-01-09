2ac564433f1772e58a629cc51aeef70417ed36ab
François Beausoleil <francois@teksol.info>
2017-01-09 14:26:39 -0500

    Prevent breakage when timezone is unset yet

--- app/helpers/format-dow.js
+++ app/helpers/format-dow.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import moment from 'moment';
 
 export function formatDow(params) {
-  let timezone  = params[0];
+  let timezone  = params[0] || 'America/Montreal';
   let timestamp = params[1];
 
   let ts = moment(timestamp);
